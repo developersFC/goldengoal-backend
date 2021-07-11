412bb6b37e93c0f9e3c097745811254ce9220c84
Saif Momani <saif.aq.momani@gmail.com>
2021-07-11 17:35:37 +0300

    starter code

---
+++ server.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+// const cors = require('cors');
+// const jwt = require('jsonwebtoken');
+// const jwksClient = require('jwks-rsa');
+// const app = express();
+// app.use(cors());
+// const PORT = process.env.PORT || 8080;
+
+mongoose.connect(
+  `mongodb+srv://saif:goldengoal@cluster0.kgujx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Connected :)');
+});
+
+const client = jwksClient({
+  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+});
+
+const getKey = (header, callback) => {
+  client.getSigningKey(header.kid, function (err, key) {
+    const signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+};
+
+app.get('/authorize', (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, getKey, {}, (err, user) => {
+    if (err) {
+      res.send('invalid token');
+    }
+    res.send(user);
+  });
+  res.send(token);
+});
+
+app.get('/rank', getRank);
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
