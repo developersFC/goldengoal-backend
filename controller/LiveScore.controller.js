2a1b22f03670f85566e0e58599c2920ef03181c1
thaerbraizat <thaerbraizat13@gmail.com>
2021-07-13 00:21:41 +0300

    livescore not f

--- controller/LiveScore.controller.js
+++ controller/LiveScore.controller.js
@@ -2,30 +2,29 @@ const { default: axios } = require('axios');
 
 const liveScore = (req, res) => {
     const { nameFav } = req.query;
-    let options = {
+    var options = {
         method: 'GET',
-        url: 'https://livescore6.p.rapidapi.com/matches/v2/list-live',
-        params: { Category: 'soccer', nameFav: nameFav },
+        url: 'https://api-football-beta.p.rapidapi.com/fixtures',
+        params: {live: 'all',nameFav:nameFav},
         headers: {
-            'x-rapidapi-key': 'd6359ff69dmshc572b5cf8548b1dp19eee8jsnf40e089d1a6f',
-            'x-rapidapi-host': 'livescore6.p.rapidapi.com'
+          'x-rapidapi-key': 'd6359ff69dmshc572b5cf8548b1dp19eee8jsnf40e089d1a6f',
+          'x-rapidapi-host': 'api-football-beta.p.rapidapi.com'
         }
-    };
-
+      };
+      
     axios.request(options).then(function (response) {
-        console.log(response.data.Stages.length);
-        for (let i = 0; i < response.data.Stages.length; i++) {
-            if (response.data.Stages[i].Events[i].T1[0].Nm === nameFav  || response.data.Stages[i].Events[i].T2[0].Nm  ===  nameFav) {
+        console.log(response.data.response);
+        for (let i = 0; i < response.length; i++) {
+            if (response[i].teams.home.name === nameFav || response[i].teams.away.name === nameFav  ) {
                 let score = {
-                    team: response.data.Stages[i].Events[i].T2[i].Nm,
-                    team2: response.data.Stages[i].Events[i].T1[i].Nm,
-                    teamGoal: response.data.Stages[i].Events[i].T2[i].Gd,
-                    team2Goal: response.data.Stages[i].Events[i].T1[i].Gd,
-                    time: response.data.Stages[i].Events[i].Eps,
-                    logoTeam1: response.data.Stages[i].Events[i].T2[i].Img,
-                    logoTeam2: response.data.Stages[i].Events[i].T1[i].Img
+                    team: response[i].teams.home.name,
+                    team2: response[i].teams.away.name,
+                    teamGoal: response[i].goals.home,
+                    team2Goal: response[i].goals.away,
+                    time: response[i].fixture.status.elapsed,
+         
                 }
-                res.send(score)
+                res.send(response)
                 console.log(score);
             } else {
                 console.error("error");
